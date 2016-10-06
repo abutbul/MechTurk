dfdb1431f5a356cec79e848dc2aac0fe19140ce0
david abutbul <david@abutbul.com>
2016-10-06 17:17:29 +0300

    ready for testing phase - form should be filled and submitted correctly

--- 2fa-browser/authmod.py
+++ 2fa-browser/authmod.py
@@ -1,16 +1,35 @@
 import os
+import time
 from selenium import webdriver # to be able to use webdriver service
-from chromeservice import chromeservice
+from selenium.webdriver.common.keys import Keys #Enter much
+from chromeservice import chromeService
+# username = ""
+# pintoken = ''
+print("Enter Username:")
+username = input()
+print("Enter PIN+Token")
+pintoken = input()
 # get pwd for local urls
 dir_path = os.path.dirname(os.path.realpath(__file__))
 # define 2fa auth url
 auth_url = "file://" + dir_path + "/../simulator/2fa-mockup.html"
 # auth_url = "http://ip/"
 capabilities = {'browserName':'chrome'}
-driverurl = chromeservice(status='start')
+driverurl = chromeService(status='start')
 driver = webdriver.Remote(driverurl, capabilities)
 driver.get(auth_url);
-
-chromeservice(status='stop')
+assert "Authentication Form" in driver.title # sanity are we in the right page?
+userelem = driver.find_element_by_name("DATA")
+userelem.clear()
+userelem.send_keys(username)
+userelem.send_keys(Keys.RETURN)
+userelem = driver.find_element_by_name("DATA")
+userelem.clear()
+userelem.send_keys(pintoken)
+userelem.send_keys(Keys.RETURN)
+submitelem = driver.find_element_by_name("submit")
+submitelem.submit()
+time.sleep(1)
+chromeService(status='stop')
 
 #
--- 2fa-browser/chromeservice.py
+++ 2fa-browser/chromeservice.py
@@ -1,16 +1,18 @@
 # this control script crontrols chromium driver service for calls
 # usage: include us and selenium webdriver
 # from selenium import webdriver
-# from chromeservice import chromeservice
-# get service url by activating service chromeservice('start')
+# from chromeservice import chromeService
+# get service url by activating service chromeService('start')
 # control chrome with webdriver.remote. consult documentation: https://sites.google.com/a/chromium.org/chromedriver/
+#HINT: http://chromedriver.storage.googleapis.com/index.html?path=2.24/ update
 # do not remove import for service from this file! altough parser may cry, it is used when called.
 import selenium.webdriver.chrome.service as service
 
-def chromeservice(status): # my first python service :)
+chromiumloc = '/usr/lib/chromium-browser/chromedriver'
+def chromeService(status): # my first python service :)
     if status in ['start', 1]: # found out python doesn't like case/switch
         global service # this is almost voodoo still learning I guess
-        service = service.Service('/usr/lib/chromium-browser/chromedriver')
+        service = service.Service(chromiumloc)
         service.start()
         global_service_url = service.service_url
         return global_service_url # this defines usage, could be probably done better. TODO
@@ -21,8 +23,8 @@ def chromeservice(status): # my first python service :)
 
 # testing grounds
 # service = service.Service('/usr/lib/chromium-browser/chromedriver')
-# chromeservice('start')
+# chromeService('start')
 # capabilities = {'browserName':'chrome'}
 # driver = webdriver.Remote(service.service_url, capabilities)
 # driver.get('http://www.google.com/xhtml');
-# chromeservice('stop')
+# chromeService('stop')
